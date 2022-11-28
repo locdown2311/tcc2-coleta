9f53e18fa723fa22949aa929c89ee777cad8545a
IGOR <igor2311@gmail.com>
2022-11-28 19:31:01 -0300

    Removido script desnecessário

--- seleciona.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-script_name=$1
-if [ -f $script_name ]; then
-    echo "O arquivo $script_name existe"
-    python ${script_name}
-else
-    echo "O arquivo $script_name não existe"
-fi
