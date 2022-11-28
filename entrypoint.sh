2576b17b55eb05a31daa710acb61a90d32f49efd
IGOR <igor2311@gmail.com>
2022-11-28 20:02:08 -0300

    Corrigido

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,9 @@ cd /usr/src/tcc/dados_brutos ;
 
 #run leituraGeral.py first
 python ../scripts/leituraGeral.py
-#run seleciona.sh for each file in /scripts, skip if leituraGeral.py
-for file in ../scripts/*; do
-    if [ "$file" != "../scripts/leituraGeral.py"]  && ["$file" != "../scripts/requirements.txt" ]; then
-        echo "Running $file"
+#run seleciona.sh for each file in /scripts, skip if leituraGeral.py and requirements.txt
+for file in /usr/src/tcc/scripts/*; do
+    if [ "$file" != "/usr/src/tcc/scripts/leituraGeral.py" ] && [ "$file" != "/usr/src/tcc/scripts/requirements.txt" ]; then
         python $file
     fi
 done
