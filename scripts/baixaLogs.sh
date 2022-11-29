d87604220416b3602ccb29ee3c91fa6dc69581d0
IGOR <igor2311@gmail.com>
2022-11-28 21:46:48 -0300

    Baixar os logs

---
+++ scripts/baixaLogs.sh
@@ -0,0 +1,51 @@
+# !bin/bash
+# Autor: Igor
+# Data: 2018-05-01
+# Descrição: Atualiza os logs do sistema
+# Versão: 1.0
+
+# Baixa os logs da ufop
+# Controladora
+scp igor@200.239.152.79:/home/logs/10.0.10.5 ./
+# APs
+# Ruckus
+scp igor@200.239.152.79:/home/logs/10.0.10.6 ./
+scp igor@200.239.152.79:/home/logs/10.0.10.29 ./
+scp igor@200.239.152.79:/home/logs/10.0.10.53 ./
+scp igor@200.239.152.79:/home/logs/10.0.10.56 ./
+scp igor@200.239.152.79:/home/logs/10.0.10.63 ./
+scp igor@200.239.152.79:/home/logs/10.0.10.68 ./
+scp igor@200.239.152.79:/home/logs/10.0.10.74 ./
+scp igor@200.239.152.79:/home/logs/10.0.10.76 ./
+scp igor@200.239.152.79:/home/logs/10.0.10.82 ./
+scp igor@200.239.152.79:/home/logs/10.0.10.101 ./
+scp igor@200.239.152.79:/home/logs/10.0.10.102 ./
+scp igor@200.239.152.79:/home/logs/10.0.10.186 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.71 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.77 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.254 ./
+# Ubiquiti
+scp igor@200.239.152.79:/home/logs/10.0.11.6 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.7 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.8 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.9 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.10 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.11 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.12 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.13 ./
+#scp igor@200.239.152.79:/home/logs/10.0.11.14 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.15 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.16 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.17 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.18 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.19 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.20 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.21 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.22 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.23 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.24 ./
+scp igor@200.239.152.79:/home/logs/10.0.11.25 ./
+
+# Radius
+scp igor@200.239.152.79:/home/logs/pfSenseRadios.icea.ufop ./
+
